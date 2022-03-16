74d8d1f039f4d80b174aeb78c537f5f654566ee9
shekhar8897 <shekhar.javaventures@gmail.com>
2022-03-16 09:02:44 -0400

    Routes Configuration

---
+++ virtual-classroom/src/constants/routes.js
@@ -0,0 +1,12 @@
+export default class Routes{
+    static SPLASHROUTE="/splash";
+    static HOMEROUTE="/home";
+    static AUTHROUTE="/auth";
+    static FACULTY_LOGINROUTE="/facultyLogin";
+    static FACULTY_SIGNUPROUTE="/facultySignup";
+    static STUDENT_LOGINROUTE="/studentLogin";
+    static STUDENT_SIGNUPROUTE="/studentSignup";
+    static HOMEROUTE="/home";
+    static PROFILEROUTE="/profile";
+    static LOGOUTROUTE="/LOGOUT"
+}
