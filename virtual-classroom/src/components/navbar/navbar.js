00c8197f2b497c5813e72179e72b4457d623ad40
Yasir Faruqui <yasir.javarndcorp@gmail.com>
2022-03-22 19:56:07 +0530

    Login & navbar added

---
+++ virtual-classroom/src/components/navbar/navbar.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import studentImg from "../../assets/images/student.png";
+import "./navbar.css";
+
+function NavBar() {
+  return (
+    <>
+      <Container>
+        <Row>
+          <Col className="studentDash">
+            <img
+              src={studentImg}
+              className="img-fluid dash-img"
+              alt="studentLogin"
+            ></img>
+            <p>Student</p>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <div className="navbar flex-column">
+              <ul>
+                <li>Home</li>
+                <li>Edit Profile</li>
+                <li>Study material</li>
+                <li>Video lectures</li>
+                <li>Q&A</li>
+                <button className="mb-2">Logout</button>
+              </ul>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
+}
+
+export default NavBar;
